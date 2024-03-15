696e68046dc80d972b1cd14a98afee79fd1d8904
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2024-03-15 17:05:11 +0100

    connection to database is established

--- server.js
+++ server.js
@@ -3,23 +3,33 @@ const { Pool } = require('pg');
 require('dotenv').config()
 
 const app = express();
-console.log(process.env.S3_BUCKET)
+
 
 const pool = new Pool ({
   user: 'postgres',
   host: 'localhost',
-  database: 'your database name!',
-  password: 'postgres',
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
   port: 5432 // Default PostgreSQL port
+});
+
+// Test the connection
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error connecting to PostgreSQL: ', err.stack);
+  } else {
+    console.log('Connected to PostgreSQL at:', res.rows[0].now);
+  }
 });
 
 // Define routes
 app.get('/', (req, res) => {
-  res.send('Hello World this is my first test');
+  res.send(`Hello World this is my first test and this is my env sage example usage ${process.env.S3_BUCKET} = ` + '${process.env.S3_BUCKET}');
 });
 
 // Start the server!
 const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`${process.env.S3_BUCKET}`);
 });
