0628946d829963724ee2049795c899dea6cf1ea5
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2024-06-04 20:28:27 +0200

    practical comment added

--- frontend/src/utils/authentication.js
+++ frontend/src/utils/authentication.js
@@ -1,3 +1,5 @@
+// Maybe this could be used later for storing authentication status instead of the "useEffect" in the header component!
+//https://redux.js.org/tutorials/quick-start
 import { createSlice } from '@reduxjs/toolkit';
 
 export const authenticationSlice = createSlice({
