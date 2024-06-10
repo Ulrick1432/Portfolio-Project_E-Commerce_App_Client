ed272278ff7f585be56cefbb6c04f5aff2111387
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2024-06-10 19:02:55 +0200

    setup for stripe used for payments

---
+++ backend/routes/checkout.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST_KEY);
+
+module.exports = (app) => {
+app.use('/checkout', router)
+
+  router.post('/create_payment_intent', async (req, res) => {
+    const { items } = req.body;
+
+    const calculateOrderAmount = (items) => {
+      // Replace this constant with a calculation of the order's amount
+      // Calculate the order total on the server to prevent
+      // people from directly manipulating the amount on the client
+      return 1400;
+    };
+
+    // Create a PaymentIntent with the order amount and currency
+    const PaymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: 'dkk',
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  })
+}
