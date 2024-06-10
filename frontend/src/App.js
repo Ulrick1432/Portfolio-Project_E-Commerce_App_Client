f13a26f937cd9ffa5abc62275491c62b79b43564
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2024-06-10 18:31:45 +0200

    setup for stripe used for payments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RegistrationPage from "./components/authentication/RegistrationPage";
@@ -8,25 +10,35 @@ import ProductPage from "./components/product/ProductPage";
 import Layout from "./components/Layout";
 import CartPage from "./components/cart";
 
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
+
 const App = () => {
 
   // sets up the router with JSX Route elements using createBrowserRouter() and createRoutesFromElements().
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route element={<Layout/>}>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path='/register' element={<RegistrationPage/>} action={registrationAction}></Route>
-        <Route path='/login' element={<LoginPage/>} action={loginAction}></Route>
-        <Route path='/product/:id' element={<ProductPage/>}></Route>
-        <Route path='/cart' element={<CartPage/>}></Route>
-      </Route>
-    </>
-  )
-);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path='/register' element={<RegistrationPage/>} action={registrationAction}></Route>
+          <Route path='/login' element={<LoginPage/>} action={loginAction}></Route>
+          <Route path='/product/:id' element={<ProductPage/>}></Route>
+          <Route path='/cart' element={<CartPage/>}></Route>
+        </Route>
+      </>
+    )
+  );
+
+  // Options used for stripe
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: '{{CLIENT_SECRET}}', // Replace with your actual client secret or pass it as a prop
+  };
 
   return (
-  <RouterProvider router={router}  />
+    <Elements stripe={stripePromise} options={options} >
+      <RouterProvider router={router}  />
+    </Elements>
   )
 };
 
